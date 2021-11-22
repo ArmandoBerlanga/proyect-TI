884bbbc17e3fdcb53206dffdc63cdd6635396317
Armando Berlanga <Jose.berlangam@udem.edu>
2021-11-22 05:03:17 -0600

    deploy

---
+++ precache-manifest.baabafc9f14bd7643944f4abbf61251c.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56c2ec192d0716ecdc14",
+    "url": "/css/about.575b5a23.css"
+  },
+  {
+    "revision": "f8d14bbcc1e78f41b107",
+    "url": "/css/app.28d4fc37.css"
+  },
+  {
+    "revision": "f6df922e8e44fb2d82e0d11a1cf68cb1",
+    "url": "/img/aboutus.f6df922e.jpg"
+  },
+  {
+    "revision": "2bf81f985a4d3d137fec99cc8eae00fb",
+    "url": "/img/account.2bf81f98.svg"
+  },
+  {
+    "revision": "b01d0a14004126a09089fa7d554a1d1e",
+    "url": "/img/banner.b01d0a14.png"
+  },
+  {
+    "revision": "6542705816ad211b0b37a71548823f2f",
+    "url": "/img/cover1.65427058.png"
+  },
+  {
+    "revision": "b0f922c634ce9d6867654ee51b04ea74",
+    "url": "/img/cover2.b0f922c6.png"
+  },
+  {
+    "revision": "a9f7ce5f0ebf8bc3ed606380c030e35a",
+    "url": "/img/danza.a9f7ce5f.png"
+  },
+  {
+    "revision": "98562f2598d2aea06c4fe7d181d92b73",
+    "url": "/img/date.98562f25.svg"
+  },
+  {
+    "revision": "d5510066c8c5588828ce8ed561d7c00b",
+    "url": "/img/email.d5510066.svg"
+  },
+  {
+    "revision": "494cb5a2f0c105a4fccf77c068578f52",
+    "url": "/img/facebook.494cb5a2.png"
+  },
+  {
+    "revision": "edbefc98541b9e4866f35afe72387cf7",
+    "url": "/img/insta.edbefc98.png"
+  },
+  {
+    "revision": "b5380ff9070992ff96e62b35dc478f1f",
+    "url": "/img/logo.b5380ff9.png"
+  },
+  {
+    "revision": "1bb9e80a29f1d159be8639e52deb4912",
+    "url": "/img/luis.1bb9e80a.png"
+  },
+  {
+    "revision": "91611b810c097e1ec2550e660a6d21ec",
+    "url": "/img/marce2.91611b81.png"
+  },
+  {
+    "revision": "b7e393db1740aac053b302cefcfddb44",
+    "url": "/img/nahomi.b7e393db.png"
+  },
+  {
+    "revision": "6ec00bd9efb93791153620c67b73e65f",
+    "url": "/img/password.6ec00bd9.svg"
+  },
+  {
+    "revision": "aa8f4c95a8018974030d1bd8c020873e",
+    "url": "/img/paula.aa8f4c95.png"
+  },
+  {
+    "revision": "789ea3aa85d592bda308d9a6adbec01d",
+    "url": "/img/paulina.789ea3aa.png"
+  },
+  {
+    "revision": "32a41e915aefe9257e2472dabb39dc72",
+    "url": "/img/phone.32a41e91.svg"
+  },
+  {
+    "revision": "eaf2e87495a40b2cd1a4afff666b1eae",
+    "url": "/img/s1.eaf2e874.jpeg"
+  },
+  {
+    "revision": "2641537448a6936f9d70d6e6c5624ec5",
+    "url": "/img/tiktok.26415374.png"
+  },
+  {
+    "revision": "b18646f05d6dc0e2e6c1729f7b352524",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56c2ec192d0716ecdc14",
+    "url": "/js/about.70bcaae2.js"
+  },
+  {
+    "revision": "f8d14bbcc1e78f41b107",
+    "url": "/js/app.e97f9030.js"
+  },
+  {
+    "revision": "367db8eab32fd4717d59",
+    "url": "/js/chunk-vendors.aa623273.js"
+  },
+  {
+    "revision": "6aadf4abf4519f5cc90f1b92e14cce35",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
