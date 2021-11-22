b35236842cbddb9fe13248f10337704fc00df94f
Armando Berlanga <Jose.berlangam@udem.edu>
2021-11-22 00:18:02 -0600

    nuevas views

--- src/router/index.js
+++ src/router/index.js
@@ -1,50 +1,36 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-const routes = [
-  {
-    path: '/',
-    name: 'Inicio',
-    component: () => import(/* webpackChunkName: "about" */ '../views/Inicio.vue')
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-  {
-    path: '/login',
-    name: 'InicioSesion',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/InicioSesion.vue')
-  },
-  {
-    path: '/clases',
-    name: 'Clases',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Clases.vue')
-  },
-  {
-    path: '/maestros',
-    name: 'Maestros',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Maestros.vue')
-  },
+const routes = [{
+        path: '/',
+        name: 'Inicio',
+        component: () =>
+            import ( /* webpackChunkName: "about" */ '../views/Inicio.vue')
+    },
+    {
+        path: '/login',
+        name: 'InicioSesion',
+        component: () =>
+            import ( /* webpackChunkName: "about" */ '../views/InicioSesion.vue')
+    },
+    {
+        path: '/clases',
+        name: 'Clases',
+        component: () =>
+            import ( /* webpackChunkName: "about" */ '../views/Clases.vue')
+    },
+    {
+        path: '/maestros',
+        name: 'Maestros',
+        component: () =>
+            import ( /* webpackChunkName: "about" */ '../views/Maestros.vue')
+    },
 
 
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+    history: createWebHistory(process.env.BASE_URL),
+    routes
 })
 
 export default router
