739f191a4be047a97e0ee379a375a4eb0ef9a1fa
Melania Coronado <melania.hdz.coronado@gmail.com>
2021-11-22 00:33:13 -0600

    popup

---
+++ src/components/Popup.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="popup">
+        <div class="popup-inner">
+            <slot />
+            <button class="popup-close" @click="TogglePopup()">
+                ACEPTAR 
+            </button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['TogglePopup']
+}
+</script>
+
+<style lang="scss" scoped>
+.popup {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: 99;
+    background-color: rgba(0, 0, 0, 0.2);
+    display: flex;
+    flex-flow: column;
+    align-items: center;
+    align-self: center;
+    justify-content: center;
+
+        &>* {
+            align-self: center;
+        }
+
+    .popup-inner {
+        background: #FFF;
+        padding: 32px;
+    }
+}
+</style>
